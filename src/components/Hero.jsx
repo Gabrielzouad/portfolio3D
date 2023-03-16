1ce213f40cb5afa76415ad7943645277f7a60987
unknown <Z20gab@gmail.com>
2023-03-16 02:01:45 +0100

    first init

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+
+const Hero = () => {
+  return (
+    <section className='w-full h-screen relative mx-auto '>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#52DE97]' />
+          <div className='w-1 sm:h-40 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}text-white`}>
+            Hei, jeg heter <span className='text-[#52DE97]'>Gabriel</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-6 text-white-100`}>Jeg er en front-end utvikler med stor interresse innen UX/UI design og Javascript </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,10 @@
+//import { Suspense, useEffect, useState } from "react"
+//import { Canvas } from "@react-three/fiber"
+//import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+// import { CanvasLoader } from "../loader"
+
+const ComputersCanvas = () => {
+  return <div></div>
+}
+
+export default ComputersCanvas
