3b0cda94193b4513d1b70dd3f42fa6f943d73b0c
unknown <Z20gab@gmail.com>
2023-03-21 02:30:10 +0100

    added about section. finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { services } from "../constants"
 import { fadeIn, textVariant } from "../utils/motion"
+import { SectionWrapper } from "../hoc"
 
 const ServiceCard = ({ title, icon, index }) => {
   return (
@@ -41,4 +42,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
