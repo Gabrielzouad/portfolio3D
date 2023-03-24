4424809edc1d6f2b7b364b822751091992089930
unknown <Z20gab@gmail.com>
2023-03-24 01:28:37 +0100

    found another import issue in earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 
-import CanvasLoader from "../loader"
+import CanvasLoader from "../Loader"
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf")
@@ -16,6 +16,7 @@ const EarthCanvas = () => {
         <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Earth />
       </Suspense>
+      <Preload all />
     </Canvas>
   )
 }
