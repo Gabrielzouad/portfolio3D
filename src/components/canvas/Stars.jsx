bf1912e4dd43620bd70f05f4765b74ceab2f7c70
unknown <Z20gab@gmail.com>
2023-03-25 04:18:52 +0100

    fix star rotation speed

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -8,13 +8,13 @@ const Stars = (props) => {
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }))
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10
-    ref.current.rotation.y -= delta / 15
+    ref.current.rotation.x -= delta / 30
+    ref.current.rotation.y -= delta / 25
   })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={10} frustumCulled {...props}>
         <PointMaterial transparent color='#f272c8' size={0.002} sizeAttenuation={true} depthWrite={false} />
       </Points>
     </group>
@@ -23,7 +23,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-auto absolute inset-0 z-[-1] overflow-hidden'>
+    <div className='w-full h-auto absolute inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
